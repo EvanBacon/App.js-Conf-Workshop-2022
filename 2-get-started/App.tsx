f11845fa04f78bc25cd70c02861e7617706f2817
evanbacon <bacon@expo.io>
2022-06-03 23:51:14 -0500

    Update App.tsx

--- 2-get-started/App.tsx
+++ 2-get-started/App.tsx
@@ -5,6 +5,7 @@ import { Step1 } from "./step-1";
 // import { Step4 } from "./step-4";
 // import { Step5 } from "./step-5";
 // import { Step6 } from "./step-6";
+// import { Step7 } from "./step-7";
 
 export default function App() {
   return (
@@ -15,6 +16,7 @@ export default function App() {
       {/* <Step4 /> */}
       {/* <Step5 /> */}
       {/* <Step6 /> */}
+      {/* <Step7 /> */}
     </View>
   );
 }
